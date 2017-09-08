5b97d8cd37a9a465c8ba05292151f3a69172ef4f
unknown <bulkguy47@gmail.com>
2017-09-08 15:48:12 +1000

    Decompose methods

--- lambda_slack_aws_billing.py
+++ lambda_slack_aws_billing.py
@@ -18,14 +18,13 @@ logger.setLevel(logging.INFO)
 client = boto3.client('cloudwatch', region_name=CLOUD_WATCH_REGION, endpoint_url='http://monitoring.'+ CLOUD_WATCH_REGION  +'.amazonaws.com')
 
 
-def get_billing():
-    start_date = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(1), datetime.time())
+def get_billing_total(start_date, end_date):
     params = {
             'MetricName': 'EstimatedCharges',
             'Namespace': 'AWS/Billing',
             'Period': 86400,
             'StartTime': start_date,
-            'EndTime': datetime.datetime.combine(datetime.date.today(), datetime.time()),
+            'EndTime': end_date,
             'Statistics': ['Maximum'],
             'Dimensions': [
                 {
@@ -42,16 +41,44 @@ def get_billing():
     billing = res['Datapoints'][0]['Maximum']
     return billing
 
+def get_billing_field(start_date, end_date, field):
+    params = {
+            'MetricName': 'EstimatedCharges',
+            'Namespace': 'AWS/Billing',
+            'Period': 86400,
+            'StartTime': start_date,
+            'EndTime': end_date,
+            'Statistics': ['Maximum'],
+            'Dimensions': [
+                {
+                    'Name': 'Currency',
+                    'Value': 'USD'
+                },
+                {
+                    'Name': 'ServiceName',
+                    'Value': field
+                }
+            ]
+    }
+    res = client.get_metric_statistics(**params)
+    print(res)
+    if (res['ResponseMetadata']['HTTPStatusCode'] != 200):
+        logger.error('Failed to get billing metrics.')
+        exit(1)
+    billing = res['Datapoints'][0]['Maximum']
+    return billing
 
-def post_slack(date, billing):
+	
+def post_slack(message):
     message = {
-            'text': "Total estimated charges for AWS04  on {} is US${}".format(date, billing)
+            'text': message,
+			'channel': SLACK_CHANNEL
             }
     req = Request(HOOK_URL, json.dumps(message))
     try:
         response = urlopen(req)
         response.read()
-        logger.info("Message posted to")
+        logger.info("Message posted to slack.")
     except HTTPError as e:
         logger.error("Request failed: %d %s", e.code, e.reason)
         exit(1)
@@ -59,8 +86,13 @@ def post_slack(date, billing):
         logger.error("Server connection failed: %s", e.reason)
         exit(1)
 
+		
+def generate_slack_message()
+	
 
 def lambda_handler(event, context):
+	start_date = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(1), datetime.time())
+	end_date = datetime.datetime.combine(datetime.date.today(), datetime.time())
     billing = get_billing()
     yesterday = datetime.date.today() - datetime.timedelta(1)
     post_slack(yesterday.strftime("%Y-%m-%d"), billing)
